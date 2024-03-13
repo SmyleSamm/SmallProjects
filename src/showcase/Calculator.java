3a1e7d42747b21c78c35cf3c42d940176540e88e
Sammy <110843478+SmyleSamm@users.noreply.github.com>
2024-03-13 23:48:48 +0000

    functional calculator

--- src/showcase/Calculator.java
+++ src/showcase/Calculator.java
@@ -19,20 +19,41 @@ public class Calculator {
             System.out.println("What shall be the second number?");
             num2=Helper.intInput();
             Helper.c();
-            
+            System.out.println("What do you want to do?\n1. addition\n2. substraction\n3. multiplication\n4. division");
+            int ans = Helper.intInput();
+            Helper.c();
+            switch(ans){
+                case 1:
+                    System.out.println("Your number is: "+addition());
+                    break;
+                case 2:
+                    System.out.println("Your number is: "+substraction());
+                    break;
+                case 3:
+                    System.out.println("Your number is: "+multiplication());
+                    break;
+                case 4:
+                    System.out.println("Your number is: "+division());
+                    break;
+                default:
+                    System.out.println("Invalid number!");
+                    break;
+            }
         }
         System.out.println("Have a great day!");
     }
     private static int addition(){
-        
+        return num1+num2;
     }
     private static int substraction(){
-
+        return num1-num2;
     }
     private static int multiplication(){
-
+        return num1*num2;
     }
-    private static int division(){
-
+    private static float division(){
+        if(num2==0)
+            return 0;
+        return (float)num1/(float)num2;
     }
 }
