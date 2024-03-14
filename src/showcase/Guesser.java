7785574fa225295589f81d6009b745c2b8bc3941
Sammy <110843478+SmyleSamm@users.noreply.github.com>
2024-03-14 12:52:43 +0000

    added functionality to Guesser

--- src/showcase/Guesser.java
+++ src/showcase/Guesser.java
@@ -1,7 +1,48 @@
 package src.showcase;
 
+import src.Helper;
+
 public class Guesser {
+    private static boolean running;
+    private static int won, loss;
     public static void main(String[] args) {
-        
+        running=true;
+        won=0;
+        loss=0;
+        Helper.c();
+        System.out.println("Welcome to the Guesser!");
+        loop();
+    }
+    private static void loop(){
+        while(running){
+            System.out.println("Enter the maximum number!\n0 interupts the programm!");
+            int ans = Helper.intInput();
+            Helper.c();
+            if(ans==0){
+                running=false;
+                break;
+            }
+            int l = new java.util.Random().nextInt(ans+1);
+            System.out.println("Your number is between and not includes 0 and "+(ans+1)+"! What number do you think it is?");
+            int answer = Helper.intInput();
+            Helper.c();
+            if(answer==l){
+                System.out.println("You guessed correctly!");
+                ++won;
+            }
+            else{
+                System.out.println("You did not guess correctly T.T\nThe number was "+l);
+                ++loss;
+            }
+            System.out.println("You have guessed "+won+" numbers correctly and "+loss+" numbers incorrectly!");
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+
+            }
+        }
+        Helper.c();
+        System.out.println("Have a great day!");
+        src.Main.start();
     }
 }
