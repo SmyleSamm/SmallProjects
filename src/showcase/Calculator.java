5bfa40d076210323db4adf38007d2c1ad03361c0
Sammy <110843478+SmyleSamm@users.noreply.github.com>
2024-03-14 12:32:00 +0000

    small changes in Calculator

--- src/showcase/Calculator.java
+++ src/showcase/Calculator.java
@@ -4,7 +4,7 @@ import src.Helper;
 
 public class Calculator {
     private static boolean running;
-    private static int num1, num2, num3;
+    private static double num1, num2, num3;
     public static void main(String[] args) {
         running=true;
         Helper.c();
@@ -13,7 +13,7 @@ public class Calculator {
     }
     public static void loop(){
         while(running){
-            System.out.println("What do you want to calculate?\n1. Easy calculations(addition to division)\n2. Complex calculations(abc calculations)");
+            System.out.println("What do you want to calculate?\n1. Easy calculations(addition to division)\n2. Complex calculations(abc calculations)\n0. Exit");
             int ans = Helper.intInput();
             Helper.c();
             switch (ans) {
@@ -22,6 +22,9 @@ public class Calculator {
                     break;
                 case 2:
                     complexCalculations();
+                    break;
+                case 0:
+                    running=false;
                     break;
                 default:
                     break;
@@ -37,7 +40,7 @@ public class Calculator {
         System.out.println("What shall be the second number?");
         num2=Helper.intInput();
         Helper.c();
-        System.out.println("What do you want to do?\n1. addition\n2. substraction\n3. multiplication\n4. division\n0. Exit");
+        System.out.println("What do you want to do?\n1. addition\n2. substraction\n3. multiplication\n4. division");
         int ans = Helper.intInput();
         Helper.c();
         switch(ans){
@@ -52,9 +55,6 @@ public class Calculator {
                 break;
             case 4:
                 System.out.println("Your number is: "+division());
-                break;
-            case 0:
-                running=false;
                 break;
             default:
                 System.out.println("Invalid number!");
@@ -73,24 +73,24 @@ public class Calculator {
         Helper.c();
         System.out.println("Your numbers are: "+abc()[0]+" and "+abc()[1]);
     }
-    private static int addition(){
+    private static double addition(){
         return num1+num2;
     }
-    private static int substraction(){
+    private static double substraction(){
         return num1-num2;
     }
-    private static int multiplication(){
+    private static double multiplication(){
         return num1*num2;
     }
-    private static float division(){
+    private static double division(){
         if(num2==0)
             return 0;
-        return (float)num1/(float)num2;
+        return num1/num2;
     }
     private static double[] abc(){
         double[] x = new double[2];
-        x[0] = ((-num2)+Math.sqrt(Math.pow(num2, 2)-4.0*(double)num1*(double)num3))/((double)num1*2);
-        x[1] = ((-num2)-Math.sqrt(Math.pow(num2, 2)-4.0*(double)num1*(double)num3))/((double)num1*2);
+        x[0] = ((-num2)+Math.sqrt(Math.pow(num2, 2)-4.0*num1*num3))/(num1*2);
+        x[1] = ((-num2)-Math.sqrt(Math.pow(num2, 2)-4.0*num1*num3))/(num1*2);
         return x;
     }
 }
