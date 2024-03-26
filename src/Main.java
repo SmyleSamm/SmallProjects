2e2f72188732f49279c78bd613d515638f3115f2
Sammy <110843478+SmyleSamm@users.noreply.github.com>
2024-03-26 00:24:02 +0000

    removed file operations from main, replaced SFR

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ package src;
 
 public class Main
 {
-    public static String showcasePath = "./src/showcase";
+    public static final String showcasePath = "./src/showcase";
     public static void main(String[] args) {
         Helper.c();
         System.out.println("Hello!\nThis is a programm for many small projects of mine!\nAll of them are controlled via the console.\nHave fun, your Samm :]");
@@ -12,7 +12,7 @@ public class Main
     public static void start(){
         printProgramms();
         System.out.println("Enter the number of the desired application!");
-        choseApplication(returnFileName(showcasePath, Helper.intInput()-1));
+        choseApplication(SFR.returnFileNamesInDirectory(showcasePath)[Helper.intInput()-1]);
     }
     //a switch statement to see what application the user wants to use. If a new application is added, u have to add it in heare and do what every u want it to do!
     public static void choseApplication(String app){
@@ -38,25 +38,15 @@ public class Main
                 break;
             default:
                 System.out.println("Invalid number, chose again!");
-                choseApplication(returnFileName(showcasePath, Helper.intInput()-1));
+                choseApplication(SFR.returnFileNamesInDirectory(showcasePath)[Helper.intInput()-1]);
         }
     }
     //it prints all the programms it can find in the showcase folder and then prints the exit thing... bro dont ask me xD
     private static void printProgramms(){
-        for(int i = 0; i < countProgramms(); ++i){
-            System.out.println((i+1)+". programm is "+returnFileName(showcasePath,i));
+        String[] programms = SFR.returnFileNamesInDirectory(showcasePath);
+        for(int i = 0; i < SFR.returnAmountInFolder(showcasePath); ++i){
+            System.out.println((i+1)+". programm is "+programms[i]);
         }
         System.out.println("0. Exit");
-    }
-    public static String returnFileName(String filePath, int index){
-        if(index>=0){
-            String names = SFR.returnFilesInFolder(filePath).get(index);
-            String[] name = names.split("\\.");
-            return name[0];
-        }else
-            return "null";
-    }
-    private static int countProgramms(){
-        return SFR.returnAmountInFolder(showcasePath);
     }
 }
