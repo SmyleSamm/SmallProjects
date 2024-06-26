fc19b19185d554e55e732894553ca091398766f1
Sammy <sambolein@gmail.com>
2024-06-26 21:30:44 +0200

    null

--- src/java/SmallProjects/showcase/Security.java
+++ src/java/SmallProjects/showcase/Security.java
@@ -10,7 +10,7 @@ public class Security
         Helper.c();
         System.out.println("Welcome to Security!");
         encrypt("user/save/Todo-List/MyNewTodo.txt");
-        decrypt("user/save/Todo-List/TestEnc.txt");
+        decrypt("test", "user/save/Todo-List/TestEnc.txt");
     }
     public static void encrypt(String file){
         String fileName = "TestEnc";
@@ -33,6 +33,25 @@ public class Security
         
         
     }
+    public static void decrypt(String file){
+        String fileName = "TestDec";
+        String filePath = "user/save/Todo-List";
+        SFR.createFile(fileName, filePath);
+        for(int j = 0; j < SFR.returnContentFromTextFile(file).length; j++){
+            String newString = new String();
+            char[] parts = SFR.returnContentFromTextFile(file)[j].toCharArray();
+            for(int i = 0; i < parts.length; i++){
+                newString += (char) ((int)parts[i]-bitIncreasement);
+            }
+            String[] oldStringFromEncFile = SFR.returnContentFromTextFile(filePath+"/"+fileName+".txt");
+            String[] newStringForEncFile = new String[oldStringFromEncFile.length+1];
+            for(int i = 0; i < oldStringFromEncFile.length; i++){
+                newStringForEncFile[i] = oldStringFromEncFile[i];
+            }
+            newStringForEncFile[oldStringFromEncFile.length] = newString;
+            SFR.writeContentToTextFile(newStringForEncFile, filePath+"/"+fileName+".txt");
+        }
+    }
     public static void decrypt(String password, String file){
         String fileName = "TestDec";
         String filePath = "user/save/Todo-List";
@@ -52,4 +71,5 @@ public class Security
             SFR.writeContentToTextFile(newStringForEncFile, filePath+"/"+fileName+".txt");
         }
     }
+
 }
