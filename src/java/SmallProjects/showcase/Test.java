40bb4e9951673259c70fc4439bfd988596a01911
Sammy <sambolein@gmail.com>
2024-06-26 21:38:07 +0200

    test class for testing stupid stuff

---
+++ src/java/SmallProjects/showcase/Test.java
@@ -0,0 +1,8 @@
+package SmallProjects.showcase;
+
+public class Test {
+    public static void main(String[] args) {
+        String name = "My name is";
+        System.out.println("\{name} Samm!");
+    }
+}
