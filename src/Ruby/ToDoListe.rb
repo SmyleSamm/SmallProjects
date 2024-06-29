1c8d57eca885d6d1d629f6852b7cb0818fcf0ccd
Sammy <sambolein@gmail.com>
2024-06-29 11:08:47 +0200

    QoL updates and introduction of saving and loading WIP

--- src/Ruby/ToDoListe.rb
+++ src/Ruby/ToDoListe.rb
@@ -1,56 +1,61 @@
 @content = []
-def listTodos
-    todos = ["exit", "view", "edit", "load", "save"]
-    for a in 0..4 do 
-        puts "#{a}. #{todos[a]} ToDo-List"
-    end 
-end
-
 def clear
     puts "\e[H\e[2J"
 end
 
 def view
+    #if empty say that its empty!!
     for x in 0..@content.length()-1
         puts "#{x+1} #{@content[x]}"
     end
 end
 
 def edit
-    listEditOption
+    #list options
+    options = ["exit", "add", "rename", "delete"]
+    for a in 0..3 do 
+        puts "#{a}. #{options[a]}"
+    end
+
     option = gets.to_i
     case option 
-    when 0 
+    when 0 #exit case
         main
-    when 1
+    when 1 #add case
         puts "Name the ToDo you want to add"
-        taskName = gets.to_s
-        @content.push(taskName)
-    when 2
+        taskName = gets.chomp.strip
+        @content.push(taskName) unless taskName.empty?
+        clear 
+    when 2 #rename case
         puts "Enter the Index of the ToDo you want to rename"
         view 
         renameAtIndex = gets.to_i 
         puts "Enter the name of the new ToDo"
         newTaskName = gets.to_s 
         @content[renameAtIndex-1] = newTaskName
-    when 3
+        clear 
+    when 3 #delete case 
         puts "Enter the Index of the ToDo you want to delete"
         view
         deleteToDoAtIndex = gets.to_i
         @content.delete_at(deleteToDoAtIndex-1)
+        clear 
     else
         puts "failed at edit"
-        edit
     end
     edit 
 end
 
 def load
-    puts "Load"
+    file_data = File.read("user/save/Todo-List/MyNewTodo1.txt").split
+    for a in 0..file_data.length-1
+        @content[a] = file_data[a]
+    end
 end
 
 def save
-    puts "Save"
+    File.write("user/save/Todo-List/MyNewTodo1.txt", @content.join("\n"))
+
 end
 
 def choseTodo(todo)
@@ -72,19 +77,19 @@ def choseTodo(todo)
         puts "See you again!"
         exit
     else
+        clear
         puts "Failed choosing Todo"
     end
     main
 end
-puts "Welcome to the ToDo-List!"
 def main
     puts "Chose what to do:"
-    #list options
-    options = ["exit", "add", "rename", "delete"]
-    for a in 0..3 do 
-        puts "#{a}. #{options[a]}"
-    end
-
+    todos = ["exit", "view", "edit", "load", "save"]
+    for a in 0..4 do 
+        puts "#{a}. #{todos[a]} ToDo-List"
+    end 
     choseTodo(gets.to_i)
 end
+clear 
+puts "Welcome to the ToDo-List!"
 main
