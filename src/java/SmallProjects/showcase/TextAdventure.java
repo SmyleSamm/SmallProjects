3b3a463b5555cdf545c36997ac4b9bfdcb069173
Sammy <110843478+SmyleSamm@users.noreply.github.com>
2024-07-07 05:26:05 +0000

    reviving da old text Adveture WIP

--- src/java/SmallProjects/showcase/TextAdventure.java
+++ src/java/SmallProjects/showcase/TextAdventure.java
@@ -5,11 +5,37 @@ import SmallProjects.Exception.IntInputException;
 public class TextAdventure {
     private static boolean running;
     private static String name;
+    private static Player player;
+    public class Town
+    {
+        String name, description;
+    }
+    public static class Player
+    {
+        String name;
+        int carisma; 
+        Player(){
+            this.name = null;
+            this.carisma = 0;
+        }
+        public String getName(){return this.name;}
+        public void setName(String name){this.name = name;}
+        public int getCarisma(){return this.carisma;}
+        public void setCarisma(int carisma){this.carisma = carisma;}
+    }
+    public static class NPC
+    {
+        int likesThePlayer;
+        NPC(){
+            likesThePlayer = new java.util.Random().nextInt(5 + player.getCarisma());
+            System.out.println(likesThePlayer);
+        }
+    }
     public static void main(String[] args) {
         running=true;
         Helper.c();
         System.out.println("Welcome to the textAdventure!");
-        introduction();
+        player = new Player();
     }
     private static void print(String str){
         System.out.println(str);
