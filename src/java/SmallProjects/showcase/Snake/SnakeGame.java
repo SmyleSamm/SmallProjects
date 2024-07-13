dc9f35804ca4649d13d8a53c96b030b750c2fc93
Sammy <sambolein@gmail.com>
2024-07-13 14:27:01 +0200

    Initial commit

---
+++ src/java/SmallProjects/showcase/Snake/SnakeGame.java
@@ -0,0 +1,12 @@
+package SmallProjects.showcase.Snake;
+import SmallProjects.Helper;
+public class SnakeGame {
+    public static void main(String[] args) {
+        Helper.c();
+        System.out.println("Welcome To Snake");
+        new SnakeGame();
+    }
+    public SnakeGame(){
+        
+    }
+}
