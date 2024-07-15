b929f99e6c84f052b743e891b4620e3222562292
Sammy <sambolein@gmail.com>
2024-07-15 15:04:16 +0200

    removed unused exception

--- src/java/SmallProjects/Exception/IntInputException.java
+++
@@ -1,7 +0,0 @@
-package SmallProjects.Exception;
-
-public class IntInputException extends Exception{
-    public IntInputException(){
-        super("Invalid Input!\nOnly numbers are accepted!");
-    }
-}
