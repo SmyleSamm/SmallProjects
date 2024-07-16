3f1fda3eee68fa8bb619de41eae07c288a24343a
Sammy <sambolein@gmail.com>
2024-07-17 00:03:18 +0200

    Tested different printing methods

--- src/java/SmallProjects/Exception/Test.java
+++ src/java/SmallProjects/Exception/Test.java
@@ -3,16 +3,24 @@ package SmallProjects.Exception;
 import SmallProjects.SFR;
 
 public class Test{
+    public static final String showcasePath = "./src/java/SmallProjects/showcase";
+    public static final String showcaseClassPath = "SmallProjects.showcase.";
     public static void main(String[] args) {
-        String showcasePath = "./src/java/SmallProjects/showcase";
-        for (String string : SFR.returnFilesInFolder(showcasePath)) {
-            System.out.println(string);
+        printPrograms();
+    }
+    private static void printPrograms(){
+        String[] programs = SFR.returnFileNamesWithoutExtensionInDirectory(showcasePath);
+        int i;
+        for(i = 0; i < programs.length; ++i){
+            System.out.println((i+1)+". program is "+programs[i]);
         }
-        System.out.println(SFR.returnFileAmountInFolder(showcasePath));
-        for(String string : SFR.returnDirectoriesInFolder(showcasePath)){
-            System.out.println(string);
+        printInDirectoryPrograms(i);
+        System.out.println("0. Exit");
+    }
+    private static void printInDirectoryPrograms(int counter){
+        String[] directories = SFR.returnDirectoriesInFolder(showcasePath);
+        for(int i = counter; i < directories.length + counter; i++){
+            System.out.println((i+1)+". program is "+directories[i-counter]);
         }
-        System.out.println(SFR.returnDirectoryAmountInFolder(showcasePath));
-        System.out.println(SFR.returnTotalAmountInFolder(showcasePath));
     }
 }
