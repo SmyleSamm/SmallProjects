37d9331575e9f4dfec4589073d40cc8a32b5af0e
Sammy <sambolein@gmail.com>
2024-07-17 00:17:03 +0200

    WIP random exception when doing random activities, save state, noted down somewhere else, do not mind this commit

--- src/java/SmallProjects/Main.java
+++ src/java/SmallProjects/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static final String showcasePath = "./src/java/SmallProjects/showcase";
     public static final String showcaseClassPath = "SmallProjects.showcase.";
+    public static String[] programs = null;
     public static void main(String[] args) {
         Helper.c();
         System.out.println("Hello!\nThis is a program for many small projects of mine!\nAll of them are controlled via the console.\nHave fun,\nYour Samm :]\n");
@@ -12,14 +13,21 @@ public class Main
     public static void start(){
         printPrograms();
         System.out.println("Enter the number of the desired application!");
-        int ans = Helper.intInputInRange(0, SFR.returnFileAmountInFolder(showcasePath));
-        if(ans>=1&&ans<=SFR.returnFileAmountInFolder(showcasePath)){
-            choseApplication(SFR.returnFileNamesWithoutExtensionInDirectory(showcasePath)[ans-1]);
-        }else{
+        int folderSize = SFR.returnTotalAmountInFolder(showcasePath);
+        int ans = Helper.intInputInRange(0, folderSize);
+        if(ans==0){
             Helper.c();
             System.out.println("See you again soon!");
             System.exit(0);
+        }else{
+            if(ans <= SFR.returnFileAmountInFolder(showcasePath)){
+                choseApplication(programs[ans-1]);
+            }else{
+            }
+            
         }
+        
+        
     }
     public static void choseApplication(String app){
         try{
@@ -30,7 +38,7 @@ public class Main
     }
     //it prints all the programs it can find in the showcase folder and then prints the exit thing... bro do not ask me xD
     private static void printPrograms(){
-        String[] programs = Helper.combineArrays(
+        programs = Helper.combineArrays(
             SFR.returnFileNamesWithoutExtensionInDirectory(showcasePath), 
             SFR.returnDirectoriesInFolder(showcasePath));
         for(int i = 0; i < programs.length; ++i){
