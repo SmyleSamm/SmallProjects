c4756d5477b0ec62e9df42f5cec0a17991050208
Sammy <sambolein@gmail.com>
2024-07-18 00:17:11 +0200

    First functionality

--- src/java/SmallProjects/showcase/Snake/Objects/Snake/SnakeBody.java
+++ src/java/SmallProjects/showcase/Snake/Objects/Snake/SnakeBody.java
@@ -1,5 +1,24 @@
 package SmallProjects.showcase.Snake.Objects.Snake;
 
-public class SnakeBody {
-    
+import SmallProjects.showcase.Snake.Objects.*;
+
+public class SnakeBody extends AbstractObject{
+    protected SnakeBody next;
+    SnakeBody(int x, int y){
+        this.next = null;
+        this.xy[0] = x;
+        this.xy[1] = y;
+    }
+    SnakeBody(int x, int y, SnakeBody next){
+        this.next = next;
+        this.xy[0] = x;
+        this.xy[1] = y;
+    }
+    protected void move(int newX, int newY){
+        System.out.println("SnakeBody: "+this.xy[0] +", "+this.xy[1]+", "+newX+", "+newY);
+        if(next!=null)
+            next.move(this.xy[0], this.xy[1]);
+        this.xy[0] = newX;
+        this.xy[1] = newY;
+    }
 }
--- src/java/SmallProjects/showcase/Snake/Objects/Snake/SnakeHead.java
+++ src/java/SmallProjects/showcase/Snake/Objects/Snake/SnakeHead.java
@@ -1,5 +1,38 @@
 package SmallProjects.showcase.Snake.Objects.Snake;
 
-public class SnakeHead {
-    
+import SmallProjects.showcase.Snake.Objects.*;
+
+public class SnakeHead extends AbstractObject{
+    private SnakeBody next;
+    public static void main(String[] args) {
+        SnakeHead sh = new SnakeHead(0, 0);
+        System.out.println("Snakehead alone");
+        sh.move(1, 0);
+        sh.move(2, 0);
+        sh.move(2, 1);
+        sh.move(2, 2);
+        SnakeHead sh1 = new SnakeHead(0, 0, new SnakeBody(-1, 0, new SnakeBody(-2, 0)));
+        System.out.println("Snakehead with two body parts");
+        sh1.move(1, 0);
+        sh1.move(2, 0);
+        sh1.move(2, 1);
+        sh1.move(2, 2);
+    }
+    SnakeHead(int x, int y){
+        this.xy[0] = x;
+        this.xy[1] = y;
+        this.next = null;
+    }
+    SnakeHead(int x, int y, SnakeBody next){
+        this.xy[0] = x;
+        this.xy[1] = y;
+        this.next = next;
+    }
+    private void move(int newX, int newY){
+        System.out.println("SnakeHead: "+this.xy[0] +", "+this.xy[1]+"; "+newX+", "+newY);
+        if(this.next!=null)
+            this.next.move(this.xy[0], this.xy[1]);
+        this.xy[0] = newX;
+        this.xy[1] = newY;
+    }
 }
