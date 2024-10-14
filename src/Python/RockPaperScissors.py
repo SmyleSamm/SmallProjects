28092d1c81a8aa619206926cdacb2a1c0938c3cb
Sammy <sambolein@gmail.com>
2024-10-14 21:09:09 +0200

    SecondVersionOfRPS

--- src/Python/RockPaperScissors.py
+++ src/Python/RockPaperScissors.py
@@ -1,26 +1,37 @@
 from random import randint
 
-coChosen = randint(0,2)
 list = ["Rock", "Paper", "Scissors"]
-print("Choose what you want to use!")
+
+def winMessage(user, opponent):
+    def printWinMessage(won, user, opponent):
+        if(won):
+            beat = "beats"
+            win = "won"
+        else:
+            beat = "is beaten by"
+            win = "lost"
+        print(f"The opponent has chosen a {list[opponent]} as their weapon. {list[user]} {beat} {list[opponent]}. You {win}!")
+
+    won = False
+
+    if(user == opponent):
+        print(f"You have both chosen the {list[user]} as your choice. It's a tie!")
+
+    else:
+        if(user==0 and opponent==2):
+            won=True
+        elif(user==1 and opponent == 0):
+            won=True
+        elif(user==2 and opponent==1):
+            won=True
+    printWinMessage(won, user, opponent)
+
+computerChoice = randint(0,2)
+
 for i in range(len(list)):
     print(f"{i}. {list[i]}")
-chosen = randint(0,2)
-print(f"You chose the {list[chosen]}")
-if chosen == coChosen:
-    print(f"You have both chosen the {list[chosen]} as your choice!")
-elif chosen==0:
-    if coChosen==2:
-        print(f"The computer chose {list[coChosen]}. You won!")
-    else:
-        print(f"The Computer chose {list[coChosen]}. You lost!")
-elif chosen==1:
-    if coChosen==0:
-        print(f"The computer chose {list[coChosen]}. You won!")
-    elif coChosen==2:
-        print(f"The Computer chose {list[coChosen]}. You lost!")
-elif chosen==2:
-    if coChosen==1:
-        print(f"The computer chose {list[coChosen]}. You won!")
-    elif coChosen==0:
-        print(f"The Computer chose {list[coChosen]}. You lost!")
+userChoice = input("Choose what you want to use!")
+#userChoice = randint(0,2)
+
+print(f"You chose the {list[userChoice]}")
+winMessage(userChoice, computerChoice)
