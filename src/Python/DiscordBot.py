15a836fb8dbd0c3474e4b027fb0ba7f9f800d477
Sammy <sambolein@gmail.com>
2024-10-19 16:21:07 +0200

    added social + changed help menu

--- src/Python/DiscordBot.py
+++ src/Python/DiscordBot.py
@@ -14,18 +14,40 @@ GUILD_ID = config.GUILD_ID()
 intents = discord.Intents.all() 
 bot = commands.Bot(command_prefix="!",
                    intents=intents,
-                   debug_guilds=[GUILD_ID])
+                   debug_guilds=[GUILD_ID],
+                   help_command=None)
 
 @bot.command()
 async def hello(ctx):
+    #mentions the user while saying hello to them
     await ctx.send(f"Hello {ctx.author.mention}!")
+
+@bot.command()
+async def social(ctx):
+    #Links of my socials
+    github = "[GitHub](https://github.com/SmyleSamm)"
+    steam = "[Steam](https://steamcommunity.com/id/SmyleSamm/)"
+    ttv = "[Twitch](https://www.twitch.tv/smyle_samm)"
+
+    #sending links to the user
+    await ctx.send(github)
+    await ctx.send(steam)
+    await ctx.send(ttv)
+
+def help_embed():
+    embed = discord.Embed(title="Help Menu", color=discord.Color.pink())
+    embed.add_field(name="!hello", value="Say hello to the bot, and the bot will greet you back", inline=False)
+    embed.add_field(name="!social", value="Get my socials", inline=False)
+    embed.add_field(name="/help or !help", value="Show this help menu", inline=False)
+    return embed
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(embed=help_embed())
 
 @bot.tree.command(name="help")
 async def help(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hello {interaction.user.mention}!\n\n"
-        "!hello: Say hello to the bot, he will say hello back to you!\n"
-        "/help: Open the help menu, just like you did right now!", 
-        ephemeral=True)
+    await interaction.response.send_message(embed=help_embed(), ephemeral=True)
 
 @bot.event
 async def on_ready():
