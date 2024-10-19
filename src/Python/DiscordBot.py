9aefb5dd209a575f2e08b05473b4b74afaee104f
Sammy <sambolein@gmail.com>
2024-10-19 20:52:54 +0200

    social actions

--- src/Python/DiscordBot.py
+++ src/Python/DiscordBot.py
@@ -2,6 +2,7 @@ import discord
 import discord.ext
 from discord.ext import commands
 import sys
+import requests
 sys.path.append("D:\\")
 import config 
 
@@ -22,24 +23,74 @@ async def hello(ctx):
     #mentions the user while saying hello to them
     await ctx.send(f"Hello {ctx.author.mention}!")
 
+def getYoutubeData(accessData: str):
+    youtube = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={config.YOUTUBE_CHANNEL_ID()}&key={config.YOUTUBE_API_KEY()}"
+    response = requests.get(youtube)
+    data = response.json()
+    
+    if 'items' in data:
+        subscribers = data['items'][0]['statistics'][accessData]
+        return subscribers
+    else:
+        return "an error occurred or channel not found!"
+    
+def getTwitchData(accessData: str):
+    return "WIP"
+
+def getGithubData(accessData: str):
+    github = f"https://api.github.com/users/SmyleSamm/{accessData}"
+    response = requests.get(github)
+    data = response.json()
+
+    return len(data)
+
 @bot.command()
-async def social(ctx, platform: str = None):
+async def social(ctx, platform: str = None, action: str = None):
+
     if platform is not None:
         platform = platform.lower()
+    if action is not None:
+        action = action.lower()
+
     social={
         "github": "https://github.com/SmyleSamm",
         "steam": "https://steamcommunity.com/id/SmyleSamm/",
-        "twitch": "https://www.twitch.tv/smyle_samm"
+        "twitch": "https://www.twitch.tv/smyle_samm",
+        "youtube": "https://www.youtube.com/@smyle_samm1372"
     }
+
+    actions={
+        "github": {"followers": getGithubData("followers"),
+                   "following": getGithubData("following")},
+        "steam": {},
+        "twitch": {"followers": getTwitchData("")},
+        "youtube": {"followers": getYoutubeData("subscriberCount"),
+                    "views": getYoutubeData("viewCount"),
+                    "videos": getYoutubeData("videoCount")}
+    }
+
     if platform is None:
         for name, link in social.items():
             await ctx.send(f"[{name.capitalize()}]({link})")
     else:
-        if platform in social:
-            await ctx.send(f"Here is the link to my [{platform}]({social[platform]})")
-        else:
+        if platform not in social:
             await ctx.send("I am sorry, but I dont linked my {platform}.\n"
-                       "You can see all my socials with !social")
+                       "You can see all my socials with !social")    
+        else:
+            if action is None:
+                await ctx.send(f"Here is the link to my [{platform}]({social[platform]})")
+
+            elif action not in actions[platform]:
+                if(actions[platform] is not None):
+                    await ctx.send(f"There is no action called: {action}.\n"
+                                   f"Supported actions for {platform} are: {", ".join(actions[platform].keys())}")
+                else:
+                    await ctx.send(f"{platform} does not support any actions.")
+            else:
+                if action in actions[platform]:
+                    await ctx.send(f"SmyleSamm has {actions[platform][action]} {action}")
+            
+            
 
 def help_embed():
     embed = discord.Embed(title="Help Menu", color=discord.Color.pink())
