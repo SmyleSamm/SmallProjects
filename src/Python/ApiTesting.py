79e35eaaa6031e389b1f35d3e18ef21c906c60aa
Sammy <sambolein@gmail.com>
2024-10-24 11:28:11 +0200

    WIP Twitch Data

---
+++ src/Python/ApiTesting.py
@@ -0,0 +1,4 @@
+import sys
+import requests
+sys.path.append("D:\\")
+import config 
--- src/Python/DiscordBot.py
+++ src/Python/DiscordBot.py
@@ -23,6 +23,38 @@ async def hello(ctx):
     #mentions the user while saying hello to them
     await ctx.send(f"Hello {ctx.author.mention}!")
 
+
+
+#DOES NOT WORK!!!!!!!!!
+@bot.command()
+async def stats(ctx, game: str = None): #if there is no game => print user statistics on current Server
+    #https://public-api.tracker.gg/api/v1/{titleSlug}/standard/profile/{platformSlug}/{platformUserIdentifier}
+    #https://public-api.tracker.gg/v2/division-2/standard/profile/{platform}/{platformUserIdentifier}
+    #trn = f"https://public-api.tracker.gg/v2/apex/standard/profile/steam/Smyle_Samm"
+    #headers = {"TRN-Api-Key": config.TRN_API_KEY()}
+    #response = requests.get(trn, headers=headers)
+    #await ctx.send(response.json())
+    #print(response.status_code)
+    #print(response.text)
+    #if response.status_code == 200:
+    #    return response.json()
+    #else:
+    #    return response.text
+    await ctx.send("At this moment, Feature is not working!")
+
+@bot.command()
+async def messagesSend(ctx, member: discord.Member, limit: int = 1000):
+    channel = ctx.channel
+    count = 0
+
+    async for message in channel.history(limit=limit):
+        if message.author == member:
+            count+=1
+
+    await ctx.send(f"{member} send {count} messages in this channel!")
+
+
+
 def getYoutubeData(accessData: str):
     youtube = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={config.YOUTUBE_CHANNEL_ID()}&key={config.YOUTUBE_API_KEY()}"
     response = requests.get(youtube)
@@ -33,10 +65,24 @@ def getYoutubeData(accessData: str):
         return subscribers
     else:
         return "an error occurred or channel not found!"
-    
-def getTwitchData(accessData: str):
-    return "{this action is WIP}"
 
+def getTwitchData(accessData: str):
+    #return "{this action is WIP}"
+    twitch = f"https://api.twitch.tv/helix/users/follows?to_id={config.TWITCH_USER_ID("smyle_samm")}"
+    headers = {
+        "Client-ID": config.TWITCH_CLIENT_ID(),
+        "Authorization": f"Bearer {config.GET_TWITCH_OAUTH_TOKEN()}"
+    }
+    response = requests.get(twitch, headers = headers)
+    print(response)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["total"]
+    else:
+        print(f"Error getting the follower count: {response.status_code}")
+        return None
+        
 def getGithubData(accessData: str):
     github = f"https://api.github.com/users/SmyleSamm/{accessData}"
     response = requests.get(github)
@@ -89,9 +135,7 @@ async def social(ctx, platform: str = None, action: str = None):
             else:
                 if action in actions[platform]:
                     await ctx.send(f"SmyleSamm has {actions[platform][action]} {action}")
-            
-            
-
+                  
 def help_embed():
     embed = discord.Embed(title="Help Menu", color=discord.Color.pink())
     embed.add_field(name="!hello", value="Say hello to the bot, and the bot will greet you back", inline=False)
